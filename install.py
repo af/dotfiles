6f9d0490cea236c56d0d05c2b3b67cead81c7c5f
Aaron Franks <aaron.franks@gmail.com>
2013-10-19 11:26:46 -0700

    Improved formatting for install script output.

--- install.py
+++ install.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 '''
 Install script for dotfiles.
 
@@ -7,21 +6,31 @@ For now, just symlinks a bunch of files to ~.
 '''
 import os
 
+# Assume this script is located the root of the dotfiles repo:
+DOTFILES_ROOT = os.path.dirname(os.path.abspath(__file__))
 SYMLINK_MAP = {
     'vim':          '~/.vim',
     'vim/vimrc':    '~/.vimrc',
     'jshintrc':     '~/.jshintrc',
 }
 
-# Assume this script is located the root of the dotfiles repo:
-DOTFILES_ROOT = os.path.dirname(os.path.abspath(__file__))
-
+linked = []
+skipped = []
 for source, dest in SYMLINK_MAP.iteritems():
     source = os.path.join(DOTFILES_ROOT, source)
     dest = os.path.abspath(os.path.expanduser(dest))
 
     if (os.path.exists(dest)):
-        print('skipped %s, file already exists' % dest)
+        skipped.append([source, dest])
     else:
+        linked.append([source, dest])
         os.symlink(source, dest)
-        print('symlinked %s to %s' % (source, dest))
+
+skipped.sort()
+linked.sort()
+for source, dest in skipped:
+    print('skipped %-40s (file already exists)' % dest)
+
+print('')
+for source, dest in linked:
+    print('symlinked %-40s to %s' % (dest, source))
