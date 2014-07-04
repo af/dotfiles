dbfd876cbaaa3460a57dcff7a41a388b7f7ac084
Aaron Franks <aaron.franks@gmail.com>
2014-07-04 10:16:21 -0700

    Replace Quicksilver app triggers with new Hydra mappings.

--- hydra/init.lua
+++ hydra/init.lua
@@ -17,9 +17,18 @@ hotkey.bind(ctrlcmd, 'N', ext.grid.topleft)
 hotkey.bind(ctrlcmd, 'M', ext.grid.bottomleft)
 hotkey.bind(ctrlcmd, ',', ext.grid.topright)
 hotkey.bind(ctrlcmd, '.', ext.grid.bottomright)
-
-hotkey.bind({"ctrl", "alt"}, 'T', function() application.launchorfocus("iTerm") end)
 hotkey.bind(ctrlaltcmd, 'R', function() repl.open(); logger.show() end)
+
+-- Shortcut to launch/focus specific apps with one keystroke.
+--
+-- Note 1: you might need to change some conflicting Mission Control keyboard
+-- shortcuts in SysPrefs > Keyboard > Shortcuts to get ^1 ^2 ^3 to work.
+-- Note 2: using launchorfocus("Google Chrome") doesn't work, so use the raw version:
+hotkey.bind({"ctrl"}, '1', function() os.execute('open -a "Google Chrome"') end)
+hotkey.bind({"ctrl"}, '2', function() application.launchorfocus("MacVim") end)
+hotkey.bind({"ctrl"}, '3', function() application.launchorfocus("iTerm") end)
+hotkey.bind({"ctrl"}, '4', function() application.launchorfocus("Slack") end)
+hotkey.bind({"ctrl"}, '0', function() application.launchorfocus("iTunes") end)
 
 
 -- simple customized menu for the OS X menubar:
