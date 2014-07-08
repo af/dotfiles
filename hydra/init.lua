182bce2e1d59d59045579a0b514a00413e712eca
Aaron Franks <aaron.franks@gmail.com>
2014-07-08 11:41:16 -0700

    Go back to old "mash" modifiers for shortcuts & reorganize hydra config.

--- hydra/init.lua
+++ hydra/init.lua
@@ -1,32 +1,39 @@
 require "grid"
 require "itunes"
 
-hydra.alert("Hydra config loaded", 0.5)
+-- TODO: replace Spark.app (volume ctrl w/ F13-15)
+-- TODO: iTunes shuffle/playlist control
 
+
+hydra.alert("Hydra is locked and loaded", 1)
 pathwatcher.new(os.getenv("HOME") .. "/.hydra/", hydra.reload):start()
 autolaunch.set(true)
 
-local ctrlaltcmd = {"ctrl", "alt", "cmd"}
-local ctrlcmd = {"ctrl", "cmd"}
+local mash = {"ctrl", "alt", "cmd"}
 
-hotkey.bind(ctrlcmd, 'K', ext.grid.fullscreen)
-hotkey.bind(ctrlcmd, 'H', ext.grid.leftchunk)
-hotkey.bind(ctrlcmd, 'L', ext.grid.rightchunk)
-hotkey.bind(ctrlcmd, 'P', ext.grid.pushwindow)
 
-hotkey.bind(ctrlcmd, 'N', ext.grid.topleft)
-hotkey.bind(ctrlcmd, 'M', ext.grid.bottomleft)
-hotkey.bind(ctrlcmd, ',', ext.grid.topright)
-hotkey.bind(ctrlcmd, '.', ext.grid.bottomright)
-hotkey.bind(ctrlaltcmd, 'R', function() repl.open(); end)
+-- Window management
+hotkey.bind(mash, 'K', ext.grid.fullscreen)
+hotkey.bind(mash, 'H', ext.grid.leftchunk)
+hotkey.bind(mash, 'L', ext.grid.rightchunk)
+hotkey.bind(mash, 'P', ext.grid.pushwindow)
 
-hotkey.bind(ctrlaltcmd, 'UP', ext.itunes.play)
-hotkey.bind(ctrlaltcmd, 'DOWN', ext.itunes.pause)
-hotkey.bind(ctrlaltcmd, 'LEFT', ext.itunes.previous)
-hotkey.bind(ctrlaltcmd, 'RIGHT', ext.itunes.next)
-hotkey.bind(ctrlaltcmd, '/', ext.itunes.currentTrack)
+hotkey.bind(mash, 'N', ext.grid.topleft)
+hotkey.bind(mash, 'M', ext.grid.bottomleft)
+hotkey.bind(mash, ',', ext.grid.topright)
+hotkey.bind(mash, '.', ext.grid.bottomright)
+hotkey.bind(mash, 'R', function() repl.open(); end)
 
--- Shortcut to launch/focus specific apps with one keystroke.
+
+-- iTunes control
+hotkey.bind(mash, 'UP', ext.itunes.play)
+hotkey.bind(mash, 'DOWN', ext.itunes.pause)
+hotkey.bind(mash, 'LEFT', ext.itunes.previous)
+hotkey.bind(mash, 'RIGHT', ext.itunes.next)
+hotkey.bind(mash, '/', ext.itunes.currentTrack)
+
+
+-- Launch/focus specific apps with one keystroke.
 -- Note: to get {^1,^2,^3} to work, you might need to change some conflicting
 -- Mission Control keyboard shortcuts in SysPrefs > Keyboard > Shortcuts
 hotkey.bind({"ctrl"}, '1', function() application.launchorfocus("Google Chrome") end)
@@ -35,8 +42,6 @@ hotkey.bind({"ctrl"}, '3', function() application.launchorfocus("iTerm") end)
 hotkey.bind({"ctrl"}, '4', function() application.launchorfocus("Slack") end)
 hotkey.bind({"ctrl"}, '0', function() application.launchorfocus("iTunes") end)
 
--- TODO: replace Spark.app (volume ctrl w/ F13-15)
--- See http://blog.coolaj86.com/articles/how-to-control-os-x-system-volume-with-applescript/
 
 -- simple customized menu for the OS X menubar:
 menu.show(function()
