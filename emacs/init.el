6bacf1eaffec95c13e1971495e479e927b702810
Aaron Franks <aaron.franks@gmail.com>
2014-09-29 12:22:38 -0700

    Add flycheck & smex for emacs.

--- emacs/init.el
+++ emacs/init.el
@@ -1,8 +1,7 @@
 ;; Package management
-;; Note: wanted to use "melpa-stable", but the evil installation failed
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
 
 ;;; from purcell/.emacs.d
 (defun require-package (package &optional min-version no-refresh)
@@ -19,12 +18,22 @@ re-downloaded in order to locate PACKAGE."
 
 (package-initialize)
 
+(require-package 'better-defaults)
+
+(require-package 'smex)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command) ;This is your old M-x.
+
 ;; js2-mode for javascript
 ;; More tips and tricks: https://github.com/cjohansen/.emacs.d/blob/master/setup-js2-mode.el
 (require-package 'js2-mode)
 (setq-default js2-strict-missing-semi-warning nil)
 (setq-default js2-global-externs '("require" "process" "$" "JSON"))
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+
+(require-package 'flycheck)
+(add-hook 'after-init-hook #'global-flycheck-mode)
 
 (require-package 'helm)
 (global-set-key (kbd "C-c h") 'helm-mini)
@@ -47,4 +56,7 @@ re-downloaded in order to locate PACKAGE."
 (load-theme 'molokai t)
 (color-theme-approximate-on)
 
-(global-linum-mode t)       ;; Show line numbers globally
+;; Misc settings
+(global-linum-mode t)         ;; Show line numbers globally
+(setq make-backup-files nil)
+(setq auto-save-default nil)  ;; TODO: "real" autosave
