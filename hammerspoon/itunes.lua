a5c9004d5962bc94d7eab8aa05df6abe46501b24
Aaron Franks <aaron.franks@gmail.com>
2015-01-06 14:29:08 -0800

    Hammerspoon: working shuffle toggle for iTunes 12

--- hammerspoon/itunes.lua
+++ hammerspoon/itunes.lua
@@ -33,27 +33,21 @@ function ext.itunes.currentTrack()
   hs.alert.show(track .. '\n' .. album .. '\n' .. artist, 1.5);
 end
 
--- NOTE: iTunes 11's applescript shuffle API is broken, so we have to do crazy
--- gymnastics just to toggle the shuffle setting on/off
--- See https://discussions.apple.com/message/22870402
+-- New shuffle implementation for iTunes 12 (the menu is different from prev. versions)
+-- adapted from https://discussions.apple.com/thread/6573883
 function ext.itunes.toggleShuffle()
-  local shuffleStatus = hs.applescript([[tell application "System Events"
-              set output to ""
-              tell application process "iTunes"
-                tell menu 1 of menu item "Shuffle" of menu "Controls" of menu bar 1
-                  set menuitems to name of menu items
-
-                  if item 1 of menuitems is "Turn On Shuffle" then
-                    click menu item 1
-                    set output to "Shuffle is ON"
-                  end if
-                  if item 1 of menuitems is "Turn Off Shuffle" then
-                    click menu item 1
-                    set output to "Shuffle is OFF"
-                  end if
-                end tell
-              end tell
-              end tell
-              return output]])
+  local succes, shuffleStatus = hs.applescript([[tell application "System Events"
+    tell application process "iTunes"
+      set shuffleOn to (value of attribute "AXMenuItemMarkChar" of menu item "On" of menu "Shuffle" of menu item "Shuffle" of menu "Controls" of menu bar 1) as string
+      if shuffleOn is "✓" then
+        click menu item "Off" of menu "Shuffle" of menu item "Shuffle" of menu "Controls" of menu bar 1
+        return "Shuffle is OFF"
+      else
+        click menu item "On" of menu "Shuffle" of menu item "Shuffle" of menu "Controls" of menu bar 1
+        return "Shuffle is ON"
+      end if
+    end tell
+    end tell
+  ]])
   hs.alert.show(shuffleStatus, 1.0)
 end
