a8d9e2bc8a2d764ae34421f8c8cbdcdbbfceb3b9
Aaron Franks <aaron.franks@gmail.com>
2015-01-09 23:09:02 -0800

    Initial implementation for hammerspoon system audio controls.

---
+++ hammerspoon/audio.lua
@@ -0,0 +1,39 @@
+local Audio = {}
+local VOLUME_MIN = 0
+local VOLUME_MAX = 100
+local VOLUME_STEP = 5
+
+-- display a unicode volume meter via hs.alert
+-- Characters found via http://changaco.net/unicode-progress-bars/
+local showVolumeAlert = function(volume)
+  -- show a 20-bar volume meter (5% increments)
+  local numBars = math.floor(volume/5)
+  local numSpaces = 20 - numBars
+  local volumeBar = string.rep('⣿', numBars) .. string.rep('⣀', numSpaces)
+  hs.alert.show(volumeBar)
+end
+
+Audio.toggleMute = function()
+  local device = hs.audiodevice.defaultOutputDevice()
+  local wasMuted = device:muted()
+  device:setMuted(not wasMuted)
+  showVolumeAlert(wasMuted and device:volume() or 0)
+end
+
+Audio.decVolume = function()
+  local device = hs.audiodevice.defaultOutputDevice()
+  local targetVolume = math.max(device:volume() - VOLUME_STEP, VOLUME_MIN)
+  device:setMuted(false)
+  device:setVolume(targetVolume)
+  showVolumeAlert(targetVolume)
+end
+
+Audio.incVolume = function()
+  local device = hs.audiodevice.defaultOutputDevice()
+  local targetVolume = math.min(device:volume() + VOLUME_STEP, VOLUME_MAX)
+  device:setMuted(false)
+  device:setVolume(targetVolume)
+  showVolumeAlert(targetVolume)
+end
+
+return Audio
--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,7 +1,7 @@
-local grid = require "grid"
-local itunes_custom = require "itunes"
+local Audio = require "audio"
+local Grid = require "grid"
+local Music = require "itunes"
 
--- TODO: replace Spark.app (volume ctrl w/ F13-15)
 
 -- Reload automatically on config changes
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", hs.reload):start()
@@ -15,15 +15,15 @@ local mash = {"ctrl", "alt", "cmd"}
 hs.hotkey.bind(mash, 'C', hs.openConsole)
 
 -- Window management
-hs.hotkey.bind(mash, 'K', grid.fullscreen)
-hs.hotkey.bind(mash, 'H', grid.leftchunk)
-hs.hotkey.bind(mash, 'L', grid.rightchunk)
-hs.hotkey.bind(mash, 'P', grid.pushwindow)
+hs.hotkey.bind(mash, 'K', Grid.fullscreen)
+hs.hotkey.bind(mash, 'H', Grid.leftchunk)
+hs.hotkey.bind(mash, 'L', Grid.rightchunk)
+hs.hotkey.bind(mash, 'P', Grid.pushwindow)
 
-hs.hotkey.bind(mash, 'N', grid.topleft)
-hs.hotkey.bind(mash, 'M', grid.bottomleft)
-hs.hotkey.bind(mash, ',', grid.topright)
-hs.hotkey.bind(mash, '.', grid.bottomright)
+hs.hotkey.bind(mash, 'N', Grid.topleft)
+hs.hotkey.bind(mash, 'M', Grid.bottomleft)
+hs.hotkey.bind(mash, ',', Grid.topright)
+hs.hotkey.bind(mash, '.', Grid.bottomright)
 
 
 -- iTunes control
@@ -32,7 +32,7 @@ hs.hotkey.bind(mash, 'DOWN', hs.itunes.pause)
 hs.hotkey.bind(mash, 'LEFT', hs.itunes.previous)
 hs.hotkey.bind(mash, 'RIGHT', hs.itunes.next)
 hs.hotkey.bind(mash, '/', hs.itunes.displayCurrentTrack)
-hs.hotkey.bind(mash, 'S', itunes_custom.toggleShuffle)
+hs.hotkey.bind(mash, 'S', Music.toggleShuffle)
 
 
 -- Launch/focus specific apps with one keystroke.
@@ -42,3 +42,9 @@ hs.hotkey.bind({"ctrl"}, '1', function() hs.application.launchOrFocus("Google Ch
 hs.hotkey.bind({"ctrl"}, '2', function() hs.application.launchOrFocus("iTerm") end)
 hs.hotkey.bind({"ctrl"}, '3', function() hs.application.launchOrFocus("iTunes") end)
 hs.hotkey.bind({"ctrl"}, '4', function() hs.application.launchOrFocus("Slack") end)
+
+
+-- Audio volume control
+hs.hotkey.bind({}, 'F13', Audio.toggleMute)
+hs.hotkey.bind({}, 'F14', Audio.decVolume)
+hs.hotkey.bind({}, 'F15', Audio.incVolume)
