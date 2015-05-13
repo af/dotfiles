3213d7f9bf86559f24a449323779fb9eb4dc6d89
Aaron Franks <aaron.franks@gmail.com>
2015-05-13 10:42:48 -0700

    Update brew_installs.sh to use vanilla vim (as a neovim fallback)

--- brew_installs.sh
+++ brew_installs.sh
@@ -14,10 +14,6 @@ reattach-to-user-namespace \
 siege \
 tig \
 tmux \
+vim \
 weechat \
 wget
-
-# Install macvim with lua support
-# Note: may need to install python via brew for this macvim to work
-# See https://github.com/Homebrew/homebrew/issues/23709
-brew install macvim --with-cscope --with-lua --with-luajit --override-system-vim
