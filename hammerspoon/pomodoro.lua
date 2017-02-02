e600c5e0a7121484e9ebe18767a0d9081693929a
Aaron Franks <aaron.franks@gmail.com>
2017-02-01 17:24:59 -0800

    pomodoro.lua: several improvements to log I/O

--- hammerspoon/pomodoro.lua
+++ hammerspoon/pomodoro.lua
@@ -6,13 +6,10 @@
 -- http://www.hammerspoon.org/docs/hs.timer.html#doEvery
 --
 -- TODO:
--- * implement pause/stop
 -- * setup timer for break time
--- * global hotkeys for all controls
--- * linebreak in log file for new day
+-- * make latest tasks alert styleable and toggleable
 
 local menu = hs.menubar.new()
-local completedPomos = {}
 local currentPomo = nil
 
 local TIMER_INTERVAL = 60       -- 60 (one minute) for real use; set lower for debugging
@@ -20,47 +17,78 @@ local POMO_LENGTH = 25          -- Length in minutes of one work interval
 local LOG_FILE = '~/.pomo'
 
 -- Namespace tables
-local Pomo = {}
+local Commands = {}
+local Log = {}
 local App = {}
 
--- TODO: move UI-related code out of the Pomo namespace
-Pomo.startNew = function()
-    local defaultName = ''
+Log.read = function(count)
+    if not count then count = 10 end
+    return hs.execute('tail -' .. count .. ' ' .. LOG_FILE)
+end
+
+Log.writeItem = function(pomo)
+    local timestamp = os.date('%Y-%m-%d %T')
+    local isFirstToday = #(Log.getCompletedToday()) == 0
+
+    if (isFirstToday) then hs.execute('echo "" >> ' .. LOG_FILE) end  -- Add linebreak between days
+    hs.execute('echo "[' .. timestamp .. '] ' .. pomo.name .. '" >> ' .. LOG_FILE)
+end
+
+Log.getLatestItems = function(count)
+    local logs = Log.read(count)
+    local logItems = {}
+    for match in logs:gmatch('(.-)\r?\n') do table.insert(logItems, match) end
+    return logItems
+end
+
+Log.getCompletedToday = function()
+    local logItems = Log.getLatestItems(20)
+    local timestamp = os.date('%Y-%m-%d')
+    local todayItems = hs.fnutils.filter(logItems, function(s)
+        return string.find(s, timestamp, 1, true) ~= nil
+    end)
+    return todayItems
+end
+
+Log.getLastTask = function()
+    local taskWithTimestamp = Log.getLatestItems(1)[1]
+    local taskStartPosition = string.find(taskWithTimestamp, ']') + 2
+    return string.sub(taskWithTimestamp, taskStartPosition)
+end
+
+Commands.startNew = function()
+    local defaultName = Log.getLastTask()
     if currentPomo then defaultName = currentPomo.name end
-    taskName = App.showDialog('what are you working on?', defaultName)
+    taskName = App.showDialog('What are you working on?', defaultName)
     if taskName then
         currentPomo = {minutesLeft=POMO_LENGTH, name=taskName}
     end
     App.updateUI()
 end
 
-Pomo.togglePaused = function()
-    hs.alert(currentPomo)
+Commands.togglePaused = function()
     if not currentPomo then return end
     currentPomo.paused = not currentPomo.paused
     App.updateUI()
 end
 
-Pomo.showLatest = function()
-    local logs = hs.execute('tail -10 ' .. LOG_FILE)
-    local displayDuration = 4
-    hs.alert(logs, displayDuration)
+Commands.showLatest = function()
+    local logs = Log.read(10)
+    local displayDuration = 5
+    hs.alert('LATEST ACTIVITY\n\n' .. logs, displayDuration)
 end
 
-Pomo.complete = function(pomo)
-    local timestamp = os.date('%Y-%m-%d %T')
-    table.insert(completedPomos, pomo)
-    hs.execute('echo "[' .. timestamp .. '] ' .. pomo.name .. '" >> ' .. LOG_FILE)
+App.complete = function(pomo)
+    Log.writeItem(pomo)
     currentPomo = nil
 end
-
 
 App.timerCallback = function()
     if not currentPomo then return end
     if currentPomo.paused then return end
     currentPomo.minutesLeft = currentPomo.minutesLeft - 1
     if (currentPomo.minutesLeft <= 0) then
-        Pomo.complete(currentPomo)
+        App.complete(currentPomo)
         hs.alert('You are done!')
         hs.execute('say "done zo"')
     end
@@ -98,12 +126,12 @@ App.init = function()
     hs.timer.doEvery(TIMER_INTERVAL, App.timerCallback)
 
     menu:setMenu(function()
-        local completedCount = #completedPomos      -- TODO: read from log file instead
+        local completedCount = #(Log.getCompletedToday())
         return {
           -- TODO: make these menu items contextual:
           { title=completedCount .. ' pomos completed today', disabled=true },
-          { title='Start', fn=Pomo.startNew },
-          { title='Pause' }
+          { title='Start', fn=Commands.startNew },
+          { title='Pause', fn=Commands.togglePaused }
         }
     end)
 
@@ -112,4 +140,4 @@ end
 
 App.init()
 
-return Pomo
+return Commands
