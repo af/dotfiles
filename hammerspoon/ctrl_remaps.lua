0a6e13aaf03931ab71139eed7b8abdfe72b29c9c
Aaron Franks <aaron.franks@gmail.com>
2017-04-26 12:12:59 -0700

    Rewrite hammerspoon ctrl remaps

--- hammerspoon/ctrl_remaps.lua
+++ hammerspoon/ctrl_remaps.lua
@@ -1,26 +1,17 @@
+local event = require('hs.eventtap').event
 local KEY_CODES = hs.keycodes.map
 
--- Override some Ctrl chord mappings to make them more ergonomic
--- In each case this changes a physically convenient mapping that terminals don't
--- support into one that the terminal will accept. As of this writing I use them for tmux
-override_handler = function(evt)
-  local modifiers = evt:getFlags()
-  local keyCode = evt:getKeyCode()
-  if not modifiers["ctrl"] then
-    return false
-  end
-
-  -- map ctrl-; to Home (used for tmux)
-  if keyCode == KEY_CODES[";"] then
-    evt:setFlags({ctrl=false})
-    evt:setKeyCode(KEY_CODES['home'])
-  end
-
-  -- map ctrl-' to End (again used for tmux)
-  if keyCode == KEY_CODES["'"] then
-    evt:setFlags({ctrl=false})
-    evt:setKeyCode(KEY_CODES['end'])
-  end
+-- Send a keystroke without the delay that hs.eventtap.keyStroke() imposes
+-- modified from https://github.com/Hammerspoon/hammerspoon/issues/1011#issuecomment-261114434
+keyStroke = function(modifiers, key)
+  event.newKeyEvent(modifiers, key, true):post()
+  event.newKeyEvent(modifiers, key, false):post()
 end
-ctrl_overrides = hs.eventtap.new({hs.eventtap.event.types.keyDown}, override_handler)
-ctrl_overrides:start()
+
+hs.hotkey.bind({'ctrl'}, KEY_CODES[';'], nil, (function()
+  keyStroke({}, KEY_CODES['home'])
+end), nil)
+
+hs.hotkey.bind({'ctrl'}, KEY_CODES["'"], nil, (function()
+  keyStroke({}, KEY_CODES['end'])
+end), nil)
