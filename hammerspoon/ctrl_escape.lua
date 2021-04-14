1b1c42b61051911951e74868bb38417d91f95e46
Aaron Franks <aaron.franks@gmail.com>
2021-04-13 20:03:26 -0700

    Updates to fix hammerspoon ctrl mapping (via https://github.com/Hammerspoon/hammerspoon/issues/1743)

--- hammerspoon/ctrl_escape.lua
+++ hammerspoon/ctrl_escape.lua
@@ -1,42 +1,31 @@
 -- Make Ctrl key dual-purpose, acting as Esc on tap
 -- This is intended to be used alongside a CapsLock that's mapped to Ctrl in OS X
--- Copied from https://github.com/jasoncodes/dotfiles/blob/master/hammerspoon/control_escape.lua
+-- Originally adapted from https://github.com/jasoncodes/dotfiles/blob/master/hammerspoon/control_escape.lua
 -- as of d619ce0 (on Oct 28, 2016)
 local send_escape = false
 local last_mods = {}
 
-local control_key_handler = function()
+local control_key_timer = hs.timer.delayed.new(0.15, function()
   send_escape = false
-end
+end)
 
-local control_key_timer = hs.timer.delayed.new(0.15, control_key_handler)
-
-local control_handler = function(evt)
+-- Need to bind to global variables to prevent garbage collection from cleaning up these eventtap bindings
+GLOBAL_FLAGSCHANGED = hs.eventtap.new({hs.eventtap.event.types.flagsChanged}, function(evt)
   local new_mods = evt:getFlags()
-  if last_mods["ctrl"] == new_mods["ctrl"] then
-    return false
-  end
+  if last_mods["ctrl"] == new_mods["ctrl"] then return false end
   if not last_mods["ctrl"] then
     last_mods = new_mods
     send_escape = true
     control_key_timer:start()
   else
-    if send_escape then
-      hs.eventtap.keyStroke({}, "ESCAPE")
-    end
+    if send_escape then hs.eventtap.keyStroke({}, "ESCAPE") end
     last_mods = new_mods
     control_key_timer:stop()
   end
   return false
-end
+end):start()
 
-local control_tap = hs.eventtap.new({hs.eventtap.event.types.flagsChanged}, control_handler)
-control_tap:start()
-
-local other_handler = function()
+GLOBAL_KEYDOWN = hs.eventtap.new({hs.eventtap.event.types.keyDown}, function()
   send_escape = false
   return false
-end
-
-local other_tap = hs.eventtap.new({hs.eventtap.event.types.keyDown}, other_handler)
-other_tap:start()
+end):start()
