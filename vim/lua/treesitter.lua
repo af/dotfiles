3ec2d9c192ce8d6becebd8bd15c967a46c77fdbd
Aaron Franks <aaron.franks@gmail.com>
2022-01-08 08:38:03 -0800

    Support for incremental_selection with <CR>

--- vim/lua/treesitter.lua
+++ vim/lua/treesitter.lua
@@ -1,6 +1,18 @@
 require('nvim-treesitter.configs').setup {
   ensure_installed = {'typescript', 'javascript', 'css', 'html', 'json', 'yaml', 'graphql', 'lua', 'rust', 'toml'},
   indent = {enable = true},
-  highlight = {enable = true}
+  highlight = {enable = true},
+
+  -- Incremental selection with <CR>! via:
+  -- https://www.reddit.com/r/neovim/comments/r10llx/the_most_amazing_builtin_feature_nobody_ever/
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = '<CR>',
+      node_incremental = '<CR>',
+      node_decremental = '<BS>',
+      -- scope_incremental = '<TAB>',
+    }
+  }
 }
 
