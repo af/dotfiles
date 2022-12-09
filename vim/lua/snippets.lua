dd1e0c6f59d4999db7de747da30dccbfc718a83a
Aaron Franks <aaron.franks@gmail.com>
2022-12-09 13:15:13 -0800

    Add basic test suite snippet

--- vim/lua/snippets.lua
+++ vim/lua/snippets.lua
@@ -1,14 +1,16 @@
+-- https://github.com/dcampos/nvim-snippy
 require('snippy').setup({
-    mappings = {
-        is = {
-            ['<Tab>'] = 'expand_or_advance',
-            ['<S-Tab>'] = 'previous',
-        },
-        nx = {
-            ['<leader>x'] = 'cut_text',
-        },
+  mappings = {
+    is = {
+      ['<Tab>'] = 'expand_or_advance',
+      ['<S-Tab>'] = 'previous',
     },
-    scopes = {
-        typescript = { '_', 'typescript', 'javascript' }
-    }
+    nx = {
+      ['<leader>x'] = 'cut_text',
+    },
+  },
+  scopes = {
+    typescript = { '_', 'typescript', 'javascript' },
+    typescriptreact = { '_', 'typescript', 'javascript' },
+  },
 })
