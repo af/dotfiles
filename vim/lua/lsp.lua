c1450ae956569ca900bcc63630382792d72c2291
Aaron Franks <aaron.franks@gmail.com>
2025-12-19 13:45:18 -0800

    Use default vim.lsp keybindings

--- vim/lua/lsp.lua
+++ vim/lua/lsp.lua
@@ -3,28 +3,19 @@ require('neodev').setup()
 
 -- For general LSP config info, see
 -- https://github.com/neovim/nvim-lspconfig
+--
+-- Attempting to stick to the default keymappings (see :h lsp-defaults):
+-- grn    -> rename symbol
+-- grr    -> list references
+-- grt    -> go to type definition
+-- gd     -> go to local definition
+-- <C-]>  -> go to global definition
 
 -- Enable completion with nvim-cmp
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- Format on save
 vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format()]])
-
-local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...)
-    vim.api.nvim_buf_set_keymap(bufnr, ...)
-  end
-
-  -- setup lsp remaps
-  local opts = { noremap = true, silent = true }
-  -- buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  -- buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'gy', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', 'gR', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', '<C-e>', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-  -- buf_set_keymap('n', '<C-w>', '<cmd>lua vim.diagnostic.set_loclist()<CR>', opts)
-end
 
 vim.lsp.enable('biome')
 vim.lsp.enable('graphql')
@@ -35,22 +26,22 @@ vim.lsp.enable('vimls')
 vim.lsp.config('ts_ls', {
   capabilities = capabilities,
   on_attach = function(client, bufnr)
-    on_attach(client, bufnr)
     -- use biome for formatting instead
     client.server_capabilities.documentFormatting = false
   end,
 })
+vim.lsp.enable('ts_ls')
 
 -- CSS
 vim.lsp.config('cssls', {
   capabilities = capabilities,
   on_attach = function(client, bufnr)
-    on_attach(client, bufnr)
     -- use biome for formatting instead
     client.server_capabilities.document_formatting = false
   end,
   filetypes = { 'css', 'scss' },
 })
+vim.lsp.enable('cssls')
 
 -- Lua
 -- Need to run `brew install lua-language-server` for support
@@ -67,17 +58,19 @@ vim.lsp.config('lua_ls', {
     },
   },
 })
+vim.lsp.enable('lua_ls')
 
+-- ruby
 vim.lsp.config('rubocop', {
   capabilities = capabilities,
   on_attach = function(client, bufnr)
-    on_attach(client, bufnr)
     client.server_capabilities.document_formatting = true
   end
 })
-vim.lsp.config('ruby_lsp', { on_attach = on_attach, capabilities = capabilities })
+vim.lsp.config('ruby_lsp', { capabilities = capabilities })
 
-vim.lsp.config('basedpyright', {
+-- python
+vim.lsp.config('ty', {
   capabilities = capabilities,
   root_markers = { 'uv.lock' },
   settings = {
@@ -89,6 +82,5 @@ vim.lsp.config('basedpyright', {
     },
   },
 })
-
-vim.lsp.enable('basedpyright')
+vim.lsp.enable('ty')
 vim.lsp.enable('ruff')
