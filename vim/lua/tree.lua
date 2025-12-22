6d159336f95f18bbf0fe6ec9706698dea0faaa83
Aaron Franks <aaron.franks@gmail.com>
2025-12-22 08:50:03 -0800

    Remove gx mapping - supported without netrw in nvim 0.11.5!

--- vim/lua/tree.lua
+++ vim/lua/tree.lua
@@ -43,20 +43,3 @@ require('nvim-tree').setup({
     },
   },
 })
-
--- netrw is disabled to help with the tree
--- The following mapping re-implements `gx` in normal mode
-function _G.open_in_browser()
-  local url = string.match(vim.fn.expand('<cWORD>'), 'https?://[%w-_%.%?%.:/%+=&]+[^ >"\',;`]*')
-  if url ~= nil then
-    if vim.fn.has('macunix') == 1 then
-      vim.cmd(('!open "%s"'):format(url))
-    else
-      vim.cmd(('!xdg-open "%s"'):format(url))
-    end
-  else
-    print('No https or http URI found in line.')
-  end
-end
-
-vim.api.nvim_set_keymap('n', 'gx', '<cmd>lua open_in_browser()<cr>', {})
